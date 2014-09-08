a7833fbaa9bfdf345f9768303a6395329eb5b7fc
scubarizzle <meineaddresse@gmx.at>
2014-09-08 22:04:09 +0200

    Update mytastybotchrome.js

--- mytastybotchrome.js
+++ mytastybotchrome.js
@@ -781,7 +781,7 @@ if (typeof tastyPlugShutDown != 'undefined') tastyPlugShutDown();
     function afkCheck() {
         if (settings.autojoin) tos.afkInt = setInterval(function(){
             if (Date.now() - afktime >= 12E5) {
-                settings.autojoin = true;
+                settings.autojoin = false;
                 $('#tp-autojoin').removeClass('button-on');
                 clearInterval(tos.afkInt);
             }
